c2e173b379b908e2aa5600e608abcdc32a6451d1
Aadarsh Jha <Aadarsh.Jha@unisys.com>
2020-04-20 00:41:49 +0530

    Updates

---
+++ precache-manifest.a782766edc7dad4f14ccad99d4d60247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed3b79ca985adbadac5881e9dedb96c",
+    "url": "/covidstats/index.html"
+  },
+  {
+    "revision": "e7a7d769d26ba0e4b277",
+    "url": "/covidstats/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d0059b13525f647eb349",
+    "url": "/covidstats/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7a7d769d26ba0e4b277",
+    "url": "/covidstats/static/js/2.201a3eb7.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/covidstats/static/js/2.201a3eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0059b13525f647eb349",
+    "url": "/covidstats/static/js/main.72d5ceac.chunk.js"
+  },
+  {
+    "revision": "f04b8c24ff9220976044",
+    "url": "/covidstats/static/js/runtime-main.2f624c86.js"
+  }
+]);
