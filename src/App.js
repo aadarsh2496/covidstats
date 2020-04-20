d9a7567df00e9b12f1682e3421e179a81ef1fc00
Aadarsh Jha <Aadarsh.Jha@unisys.com>
2020-04-20 18:37:19 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React,{useEffect,useState} from 'react';
 import './App.css';
+import './indiaCases'
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
-import Moment from 'react-moment';
 import 'moment-timezone';
 import Columns from "react-columns";
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
 function formatDate(string){
     var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
     return new Date(string).toLocaleDateString([],options);
@@ -31,8 +31,6 @@ axios
     console.log(err);
   });
 },[]);
-const date = new Date(parseInt(latest.updated));
-const lastUpdated = date.toString();
 //filter country's countryInfo Begin
 const filterCountries = results.filter(
   item =>{
@@ -41,6 +39,39 @@ const filterCountries = results.filter(
 );
 //filter country's countryInfo End
 const countries = filterCountries.map((data, index) => {
+  if(data.country.toLowerCase() === "india")
+  {
+    return(
+      <Card
+      key = {index}
+      bg="light"
+      text={"dark"}
+      className="text-center"
+      style={{ margin: "10px" }}
+      >
+      <Card.Img variant = "top" src ={data.countryInfo.flag}/>
+      <Card.Body>
+        <Card.Title>{data.country.toUpperCase()}</Card.Title>
+        <Card.Text>Total Cases: {data.cases}</Card.Text>
+        <Card.Text>Deaths reported: {data.deaths}</Card.Text>
+        <Card.Text>Recovered cases: {data.recovered}</Card.Text>
+        <Card.Text>Today's Cases: {data.todayCases}</Card.Text>
+        <Card.Text>Today's Deaths: {data.todayDeaths}</Card.Text>
+        <Card.Text>Active Cases: {data.active}</Card.Text>
+        <Card.Text>Critical Cases: {data.critical}</Card.Text>
+        <Card.Text>Cases per OneMillion: {data.casesPerOneMillion}</Card.Text>
+        <Card.Text>Deaths per OneMillion: {data.deathsPerOneMillion}</Card.Text>
+        <Card.Text>Total Tests conducted: {data.tests}</Card.Text>
+        <Card.Text>Tests Per OneMillion: {data.testsPerOneMillion}</Card.Text>
+        <Card.Text>Visit(http://aadarsh2496.github.io/covidinindia/) to view state wise case.</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small>Last updated: {formatDate(data.updated)}.</small>
+      </Card.Footer>
+      </Card>
+    );
+  }
+  else{
   return(
     <Card
     key = {index}
@@ -59,12 +90,17 @@ const countries = filterCountries.map((data, index) => {
       <Card.Text>Today's Deaths: {data.todayDeaths}</Card.Text>
       <Card.Text>Active Cases: {data.active}</Card.Text>
       <Card.Text>Critical Cases: {data.critical}</Card.Text>
+      <Card.Text>Cases per OneMillion: {data.casesPerOneMillion}</Card.Text>
+      <Card.Text>Deaths per OneMillion: {data.deathsPerOneMillion}</Card.Text>
+      <Card.Text>Total Tests conducted: {data.tests}</Card.Text>
+      <Card.Text>Tests Per OneMillion: {data.testsPerOneMillion}</Card.Text>
     </Card.Body>
     <Card.Footer>
       <small>Last updated: {formatDate(data.updated)}.</small>
     </Card.Footer>
     </Card>
   );
+}
 });
 var queries = [{
     columns: 2,
@@ -73,6 +109,7 @@ var queries = [{
     columns: 3,
     query: 'min-width: 1000px'
   }];
+  //var ViewCount = count.ViewCount;
   return (
     <div>
     <br/>
@@ -87,7 +124,10 @@ style={{ margin: "10px" }}
   <Card.Body>
     <Card.Title>World Cases</Card.Title>
     <Card.Text>
-      {latest.cases}
+      Total: {latest.cases}
+    </Card.Text>
+    <Card.Text>
+      Today's Cases Resported: {latest.todayCases}
     </Card.Text>
   </Card.Body>
   <Card.Footer>
@@ -104,6 +144,9 @@ style={{ margin: "10px" }}
     <Card.Title>World Deaths</Card.Title>
     <Card.Text>
       {latest.deaths}
+    </Card.Text>
+    <Card.Text>
+      Today's Deaths Resported: {latest.todayDeaths}
     </Card.Text>
   </Card.Body>
   <Card.Footer>
@@ -132,7 +175,7 @@ style={{ margin: "10px" }}
   <Form.Group controlId="formGroupSearch">
     <Form.Control
     type="text"
-    placeholder="Enter Country's name"
+    placeholder="Filter by Country's name"
     onChange = {e=> setSearchCountries(e.target.value.toLowerCase())}
     style={{ width: "230px" }}
     />
@@ -143,5 +186,4 @@ style={{ margin: "10px" }}
     </div>
   );
 }
-
 export default App;
